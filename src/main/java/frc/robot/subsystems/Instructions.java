b214e6fd4c7fa84a339daba0f691bda33396630c
andy <24chenay@gmail.com>
2022-11-08 13:28:05 -0800

    11/8 changes

--- src/main/java/frc/robot/subsystems/Instructions.java
+++ src/main/java/frc/robot/subsystems/Instructions.java
@@ -1,33 +1,30 @@
 package frc.robot.subsystems;
 import frc.robot.Constants;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class Instructions extends SubsystemBase {
-    private final RelativeEncoder frontLeftEncoder;
-    private final RelativeEncoder frontRightEncoder;
-    private final RelativeEncoder rearLeftEncoder;
-    private final RelativeEncoder rearRightEncoder;
-    private final CANSparkMax frontLeft;
-    private final CANSparkMax frontRight;
-    private final CANSparkMax rearLeft;
-    private final CANSparkMax rearRight;
+    public final CANSparkMax c;
+    public final DigitalInput digitalSwitch;
     
     public Instructions(){
-        this.frontLeft = new CANSparkMax(Constants.frontLeftMotorPort, MotorType.kBrushless);
-        this.frontRight = new CANSparkMax(Constants.frontRightMotorPort, MotorType.kBrushless);
-        this.rearLeft = new CANSparkMax(Constants.rearLeftMotorPort, MotorType.kBrushless);
-        this.rearRight = new CANSparkMax(Constants.rearRighttMotorPort, MotorType.kBrushless);
-        this.frontLeftEncoder = this.frontLeft.getEncoder();
-        this.frontRightEncoder = this.frontRight.getEncoder();
-        this.rearLeftEncoder = this.rearLeft.getEncoder();
-        this.rearRightEncoder = this.rearRight.getEncoder();
+       this.c = new CANSparkMax(Constants.climberArmMotorPort, MotorType.kBrushless);
+       this.digitalSwitch = new DigitalInput(Constants.angleSwitchPort);
     }
     public void resetEncoder(){
          
   }
+    public void setArmSpeed(double input)
+    {
+      c.set(input);
+
+    }
+    public boolean switchIsPressed(){
+      return(digitalSwitch.get());
+    }
+
 
 }
